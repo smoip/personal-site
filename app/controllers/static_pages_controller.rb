944d34bffabcc44204af6ac3607a67bd8f90fa6c
smoip <drsmoip@gmail.com>
2015-01-26 13:12:18 -0600

    generate static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def software
+  end
+
+  def music
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/software'
+
+  get 'static_pages/music'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
